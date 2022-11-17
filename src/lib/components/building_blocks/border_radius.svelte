83e99ee62cd3dcd9de2fbfa6fd03bd144bf1f6da
yifan117 <yifan.qiu17@gmail.com>
2022-11-17 16:21:32 +1030

    converted border-radius.svelte to stylus

--- src/lib/components/building_blocks/border_radius.svelte
+++ src/lib/components/building_blocks/border_radius.svelte
@@ -39,63 +39,48 @@
     </div>
 </div>
 
-<style>
+<style lang='stylus'>
+    .container
+    label
+    .padding_icon
+    .padding_text
+        display flex
+        align-items center
+        justify-content space-between
+        gap 8px
 
-    .container {
-        display: flex;
-        align-items: center;
-        justify-content: space-between;
-        width: 100%;
-        gap: 6px;
-    }
+    .container
+        width 100%
+        gap 6px
 
-    label {
-        display: flex;
-        gap: 8px;
-        display: flex;
-        align-items: center;
-        justify-content: space-between;
-        padding: 4px;
-        border-radius: 3px;
-    }
+    label
+        padding 4px
+        border-radius 3px
 
-    label:hover {
-        outline: 1.5px solid rgba(127, 17, 224, 0.3);
-    }
+        &:hover
+            outline 1.5px solid rgba(127, 17, 224, 0.3)
 
-    label:focus-within {
-        outline: 1.5px solid var(--brand-purple);
-    }
+        &:focus-within
+            outline 1.5px solid var(--brand-purple)
 
-    input::-webkit-outer-spin-button, input::-webkit-inner-spin-button {
-        -webkit-appearance: none;
-        margin: 0;
-    }
+    input::-webkit-outer-spin-button
+    input::-webkit-inner-spin-button
+        appearance none
+        margin 0
 
-    input[type=number] {
-        -moz-appearance: textfield;
-    }
+    input[type=number]
+        appearance textfield
 
-    input {
-        text-align: center;
-        max-width: 20px;
-        border: none;
-        border-radius: 3px;
-    }
+    input
+        text-align center
+        max-width 20px
+        border none
+        border-radius 3px
 
-    input:focus {
-        outline: none;
-    }
+        &:focus
+            outline none
 
-    .padding_icon, .padding_text {
-        display: flex;
-        align-items: center;
-        justify-content: space-between;
-    }
-
-    .padding_text {
-        font-size: 14px;
-        font-weight: 400;
-    }
-
+    .padding_text
+        font-size 14px
+        font-weight 400
 </style>
