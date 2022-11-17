c3086b955f4acdd9acb8ba692504096fc32d2271
yifan117 <yifan.qiu17@gmail.com>
2022-11-17 16:16:00 +1030

    converted align_container.svelte to stylus

--- src/lib/components/building_blocks/align_container.svelte
+++ src/lib/components/building_blocks/align_container.svelte
@@ -5,44 +5,41 @@
 
 <div class="align_container">
     {#each Array(3) as _}
-        <div class="grid_box">
+        <div class="grid box">
             <SquareSmall/>
         </div>
     {/each}
 
-    <div class="special_grid_box">
+    <div class="grid box special">
         <AutoLayoutBars size='22px' color='var(--brand-purple)'/>
     </div>
 
     {#each Array(5) as _}
-        <div class="grid_box">
+        <div class="grid box">
             <SquareSmall/>
         </div>
     {/each}
 </div>
 
 <style lang='stylus'>
-$color-1 = rgba(33, 37, 45, 0.1)
+    $color-1 = rgba(33, 37, 45, 0.1)
 
-.align_container
-    display grid
-    grid-template-rows repeat(3, 1fr)
-    grid-template-columns repeat(3, 1fr)
-    aspect-ratio 1
-    border 2px solid $color-1
-    border-radius 2px
+    .align_container
+        display grid
+        grid-template-rows repeat(3, 1fr)
+        grid-template-columns repeat(3, 1fr)
+        aspect-ratio 1
+        border 2px solid $color-1
+        border-radius 2px
 
-.special_grid_box
-    display flex
-    align-items center
-    justify-content center
-    aspect-ratio 1
-    transform rotate(90deg)
+    .grid.box, .grid.box.special
+        aspect-ratio 1
+        opacity 10%
+        display flex
+        align-items center
+        justify-content center
 
-.grid_box
-    aspect-ratio 1
-    opacity 10%
-    display flex
-    align-items center
-    justify-content center
+    .grid.box.special
+        transform rotate(90deg)
+        opacity 100%
 </style>
